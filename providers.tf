655f1742a3e16636948c6c8444bd0e2d746a5eec
moniskhantelus <93679325+moniskhantelus@users.noreply.github.com>
2025-12-10 16:10:50 -0700

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,3 +1,11 @@
-provider "aws" {
+terraform {
+  backend "s3" {
+    bucket         = "monis-terraform-backend"
+    key            = "cicdaws/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
+  }
+}provider "aws" {
   region = "us-east-1"
 }
